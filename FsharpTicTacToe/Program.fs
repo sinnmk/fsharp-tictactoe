b7152cc0f0485bf113081ffb6db6b56a133044b6
Michelle Sinn <34252468+sinnmk@users.noreply.github.com>
2018-11-19 16:57:58 -0600

    wip

--- FsharpTicTacToe/Program.fs
+++ FsharpTicTacToe/Program.fs
@@ -25,22 +25,24 @@ let main args =
             printfn("Error, not correct input")
         if(checkForWin (board) marker = true) then  
             printfn"Winner"
+            Environment.Exit(-1)
         printBoard board
         i <-i + 1
 
         System.Threading.Thread.Sleep(1000)
 
-        //printfn "Please enter your move (1-9)"
-        //let mutable move =  Console.ReadLine() |> int 
-        //let mutable marker = "O" 
-        //if (isAvailablePosition (board) move = true) then
-        //    board.[move-1] <- marker
-        //else 
-        //    printfn("Error, not correct input")
-        //if(checkForWin (board) marker = true) then  
-        //    printfn"Winner"
-        //printBoard board
-        //i <-i + 1
+        printfn "Please enter your move (1-9)"
+        let mutable move =  Console.ReadLine() |> int 
+        let mutable marker = "O" 
+        if (isAvailablePosition (board) move = true) then
+            board.[move-1] <- marker
+        else 
+            printfn("Error, not correct input")
+        if(checkForWin (board) marker = true) then  
+            printfn"Winner"
+            Environment.Exit(-1)
+        printBoard board
+        i <-i + 1
 
     Console.ReadKey() |> ignore
     0 
